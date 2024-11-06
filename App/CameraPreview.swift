b033c3993931c99daa3b13ef789fbf0722f644c4
Graham Reeves <graham@grahamreeves.com>
2024-11-06 17:09:32 +0000

    Making and rendering a texture!

--- App/CameraPreview.swift
+++ App/CameraPreview.swift
@@ -46,6 +46,46 @@ let QuadVertexes =
 ]
 
 
+func GetTestTexture(device:Device) -> WebGPU.Texture
+{
+	let kTextureWidth = UInt32(5);
+	let kTextureHeight = UInt32(7);
+	let size = Extent3d(width: kTextureWidth,height: kTextureHeight)
+
+	let o : [UInt8] = [255,   0,   0, 255];  // red
+	let y : [UInt8] = [255, 255,   0, 255];  // yellow
+	let b : [UInt8] = [  0,   0, 255, 255];  // blue
+	let textureData : [[UInt8]] = [
+		b, o, o, o, o,
+		o, y, y, y, o,
+		o, y, o, o, o,
+		o, y, y, o, o,
+		o, y, o, o, o,
+		o, y, o, o, o,
+		o, o, o, o, o,
+	]
+	let textureDataFlat = textureData.flatMap{$0}
+	
+	let description = TextureDescriptor(label: "TestTexture",
+										usage: TextureUsage(rawValue: TextureUsage.textureBinding.rawValue|TextureUsage.copyDst.rawValue),
+										size: size,
+										format:TextureFormat.rgba8Unorm
+										)
+	let texture = device.createTexture(descriptor: description)
+
+	let layout = TextureDataLayout(offset: 0,bytesPerRow: kTextureWidth*4)
+
+	//	copy-this-texture instructoin
+	let copyMeta = ImageCopyTexture(texture: texture)
+	
+	textureDataFlat.withUnsafeBytes
+	{
+		UnsafeRawBufferPointer in
+			device.queue.writeTexture(destination: copyMeta, data: UnsafeRawBufferPointer, dataLayout: layout, writeSize: size )
+	}
+	
+	return texture
+}
 
 
 class CameraPreviewInstance
@@ -54,10 +94,20 @@ class CameraPreviewInstance
 	var pipeline : WebGPU.RenderPipeline?
 	var vertexBuffer : WebGPU.Buffer?
 	var vertexCount : UInt32?
+	var texture : Texture?
+	var sampler : Sampler?
+	var bindGroup : BindGroup?
 	
 	//	can we get this from the surface view?
 	var windowTextureFormat = TextureFormat.bgra8Unorm
 
+	func InitTexture(device:Device)
+	{
+		self.texture = GetTestTexture(device: device)
+		self.sampler = device.createSampler()
+
+	}
+	
 	
 	func InitResources(device:Device)
 	{
@@ -66,6 +116,8 @@ class CameraPreviewInstance
 		{
 			return
 		}
+		
+		InitTexture(device: device)
 		
 		let vertexShaderSource = """
 		struct VertexOut {
@@ -88,10 +140,13 @@ class CameraPreviewInstance
 		"""
 		
 		let fragmentShaderSource = """
+		@group(0) @binding(0) var ourSampler: sampler;
+		@group(0) @binding(1) var ourTexture: texture_2d<f32>;
 		@fragment fn main(
 			@location(0) uv : vec2<f32>
 		) -> @location(0) vec4<f32> 
 		{
+			return textureSample(ourTexture, ourSampler, uv );
 			return vec4( uv, 0, 1 );
 		}
 		"""
@@ -113,7 +168,19 @@ class CameraPreviewInstance
 			buffers: [Vertex2.layout]
 		)
 		
+		let bindGroupLayout = device.createBindGroupLayout(descriptor: BindGroupLayoutDescriptor(
+			entries: [
+				BindGroupLayoutEntry(binding: 0,visibility: .fragment,sampler: SamplerBindingLayout(type:.nonFiltering)),
+				BindGroupLayoutEntry(binding: 1,visibility: .fragment,texture: TextureBindingLayout(sampleType: .float))
+				//BindGroupLayoutEntry(binding: 1,visibility: .fragment,buffer: BufferBindingLayout(type: .uniform))
+			]
+		))
+		
+		let pipelineLayout = device.createPipelineLayout(descriptor: PipelineLayoutDescriptor(
+			bindGroupLayouts: [bindGroupLayout]))
+		
 		let pipelineDescription = RenderPipelineDescriptor(
+			layout:pipelineLayout,
 			vertex: vertexDescription,
 			fragment: FragmentState(
 				module: fragmentShader,
@@ -122,6 +189,23 @@ class CameraPreviewInstance
 					ColorTargetState(format: windowTextureFormat)
 				]))
 		self.pipeline = device.createRenderPipeline(descriptor:pipelineDescription)
+		guard let pipeline else
+		{
+			//throw RuntimeError("Failed to make pipeline")
+			return
+		}
+		
+		let bindMeta = BindGroupDescriptor(label: "Texture bind",
+			layout: pipeline.getBindGroupLayout(groupIndex:0),
+			entries: [
+				BindGroupEntry( binding: 0, sampler: self.sampler! ),
+				BindGroupEntry( binding: 1, textureView: self.texture!.createView() )
+			]
+		)
+		
+		self.bindGroup = device.createBindGroup(descriptor: bindMeta)
+		
+		
 		
 		self.vertexCount = UInt32(QuadVertexes.count)
 		self.vertexBuffer = QuadVertexes.withUnsafeBytes { vertexBytes -> Buffer in
@@ -152,6 +236,7 @@ class CameraPreviewInstance
 					storeOp: .store,
 					clearValue: WebGPU.Color(r: 0, g: 1, b: 1, a: 1))]))
 		renderPass.setPipeline(pipeline)
+		renderPass.setBindGroup(groupIndex: 0,group:bindGroup)
 		renderPass.setVertexBuffer(slot: 0, buffer: vertexBuffer!)
 		renderPass.draw(vertexCount: self.vertexCount!)
 		renderPass.end()
